56d2fb0b0bc12c8304619340cdbcc1fa62dede89
Charlee Cordts <ckcordts@gmail.com>
2019-11-05 10:36:11 -0700

    Website

---
+++ app.js
@@ -0,0 +1,9 @@
+//alert("Now in my own flie");
+
+
+ document.body.style.background = "red";
+
+ setTimeout(() => {
+    //document.body.style.background = "white";
+    console.log("Hi");
+   },2000);
